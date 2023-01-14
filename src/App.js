27a06652be7de2977f19b9de14ac3e4c3640549c
Afzal00786 <afzalyousaf497@gmail.com>
2023-01-14 00:37:05 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import BlogPage from "../src/components/BlogPage";
+import Home from "../src/components/HomePage";
+import Article from "./components/Artical"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Category_Detail from "../src/components/Cetogry"
+import ProductDetail from "../src/components/productDetail"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/article" element={<Article />} />
+        <Route path="/article" element={<Article />} />
+        <Route path="/category" element={<Category_Detail />} />
+        <Route path="/product-detail" element={<ProductDetail />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
